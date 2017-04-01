97342e34f4aa7a6513bad05577d159434d4fe63b
fagguy <lim.isaiah@gmail.com>
2017-04-01 16:30:11 +0800

    Added functionality to read input from console

--- BinaryStringRecognition/BinaryStringRecognition.cpp
+++ BinaryStringRecognition/BinaryStringRecognition.cpp
@@ -2,26 +2,7 @@
 
 int main()
 {
-	PseudoTree tree;
-	string test = "";
-
-	test = "00111";
-	tree.AddStringToBranch(test);
-	test = "10110";
-	tree.AddStringToBranch(test);
-	test = "11101";
-	tree.AddStringToBranch(test);
-	test = "11100";
-	tree.AddStringToBranch(test);
-	test = "11011";
-	tree.AddStringToBranch(test);
-	test = "01010";
-	tree.AddStringToBranch(test);
-	test = "00001";
-	tree.AddStringToBranch(test);
-	test = "00000";
-	tree.AddStringToBranch(test);
-	
+	GetInput();
 	tree.PrintContents();
 
 	cout << "min no. of identity bits: " << endl
@@ -29,4 +10,18 @@ int main()
 
 	system("Pause");
 	return 0;
+}
+
+void GetInput()
+{
+	unsigned int K;
+	string binaryString;
+
+	cin >> K;
+
+	for (unsigned int i = 0; i < K; ++i)
+	{
+		cin >> binaryString;
+		tree.AddStringToBranch(binaryString);
+	}
 }
--- BinaryStringRecognition/BinaryStringRecognition.h
+++ BinaryStringRecognition/BinaryStringRecognition.h
@@ -1,3 +1,7 @@
 #pragma once
 
 #include "PseudoTree.h"
+
+PseudoTree tree;
+
+void GetInput();
